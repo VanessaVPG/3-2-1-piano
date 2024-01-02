14e5c7e265171bac956fb59cacd95e6f0e773524
vanessavpg <vanessa.pg026@gmail.com>
2024-01-02 13:21:52 -0300

    feat: piano

---
+++ src/scripts/engine.js
@@ -0,0 +1,43 @@
+const pianoKeys = document.querySelectorAll('.piano-keys .key');
+
+const volumeSlider = document.querySelector('.volume-slider input');
+
+const keysCheck = document.querySelector('.keys-toggle input');
+
+let mapedKeys = [];
+let audio = new Audio('./src/tunes/a.wav');
+
+const playTune = (key) => {
+  audio.src = `./src/tunes/${key}.wav`;
+  audio.play();
+  const clickedKey = document.querySelector(`[data-key="${key}"]`)
+  clickedKey.classList.add('active');
+  setTimeout(() => {
+    clickedKey.classList.remove('active')
+  }, 150)
+}
+
+pianoKeys.forEach((key) => {
+  key.addEventListener("click", () => playTune(key.dataset.key));
+  mapedKeys.push(key.dataset.key);
+})
+
+document.addEventListener("keydown", (e) => {
+  if(mapedKeys.includes(e.key)) {
+    playTune(e.key);
+  }
+});
+
+handleVolumeChange = (e) => {
+  audio.volume = e.target.value / 100;
+}
+
+volumeSlider.addEventListener('input', handleVolumeChange);
+
+const showHideKeys = (e) => {
+ pianoKeys.forEach((key) => key.classList.toggle('hide'));
+ console.log(e.target.checked);
+}
+
+
+keysCheck.addEventListener('click', showHideKeys);
